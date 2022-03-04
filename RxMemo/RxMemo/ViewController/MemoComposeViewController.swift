826eeb22fac4b05403da35aba25240f30d48bd70
Choyoonyoung98 <yoon980208@gmail.com>
2022-03-04 16:36:59 +0900

    [CREATE] 필요 Viewcontroller 목록 파일 생성

---
+++ RxMemo/RxMemo/ViewController/MemoComposeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MemoComposeViewController.swift
+//  RxMemo
+//
+//  Created by 조윤영 on 2022/03/04.
+//
+
+import UIKit
+
+class MemoComposeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ RxMemo/RxMemo/ViewController/MemoListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MemoListViewController.swift
+//  RxMemo
+//
+//  Created by 조윤영 on 2022/03/04.
+//
+
+import UIKit
+
+class MemoListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
