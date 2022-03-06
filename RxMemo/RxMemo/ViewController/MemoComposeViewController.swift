74782245b1d828fe6a485b0d9da8bb2c495f8878
Choyoonyoung98 <yoon980208@gmail.com>
2022-03-06 22:15:11 +0900

    [UPDATE] 메모 쓰기 화면 구성 및 ViewModel과 View 바인딩 처리

--- RxMemo/RxMemo/ViewController/MemoComposeViewController.swift
+++ RxMemo/RxMemo/ViewController/MemoComposeViewController.swift
@@ -6,15 +6,54 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import Action
+import NSObject_Rx
 
 class MemoComposeViewController: UIViewController, ViewModelBindableType {
-
+    @IBOutlet weak var cancelButton: UIBarButtonItem!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    @IBOutlet weak var contentTextView: UITextView!
+    
     var viewModel: MemoComposeViewModel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        contentTextView.becomeFirstResponder()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        if contentTextView.isFirstResponder {
+            contentTextView.resignFirstResponder()
+        }
+    }
 
     func bindViewModel() {
+        viewModel.title
+            .drive(navigationItem.rx.title)
+            .disposed(by: rx.disposeBag)
+        
+        viewModel.initialText
+            .drive(contentTextView.rx.text)
+            .disposed(by: rx.disposeBag)
+        
+        cancelButton.rx.action = viewModel.cancelAction
+        
+        //1: 더블클릭과 같은 중복 탭이 발생할 수 있으므로 0.5초마다 처리
+        //2. withLatestFrom: textView에 입력되어 있는 텍스트 방출
+        //3. saveAction에 있는 입력값으로 바인딩
+        saveButton.rx.tap
+            .throttle(.milliseconds(500), scheduler: MainScheduler.instance)
+            .withLatestFrom(contentTextView.rx.text.orEmpty)
+            .bind(to: viewModel.saveAction.inputs)
+            .disposed(by: rx.disposeBag)
     }
 }
