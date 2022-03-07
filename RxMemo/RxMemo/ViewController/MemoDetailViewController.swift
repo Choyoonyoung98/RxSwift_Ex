1ff1c1023b1c58cce75ce685f3ea10beda9504d0
Choyoonyoung98 <yoon980208@gmail.com>
2022-03-07 18:55:17 +0900

    [UPDATE] 메모 보기 화면 구현

--- RxMemo/RxMemo/ViewController/MemoDetailViewController.swift
+++ RxMemo/RxMemo/ViewController/MemoDetailViewController.swift
@@ -8,6 +8,12 @@
 import UIKit
 
 class MemoDetailViewController: UIViewController, ViewModelBindableType {
+    @IBOutlet weak var contentTableView: UITableView!
+    
+    @IBOutlet weak var deleteButton: UIBarButtonItem!
+    @IBOutlet weak var editButton: UIBarButtonItem!
+    @IBOutlet weak var shareButton: UIBarButtonItem!
+    
     var viewModel: MemoDetailViewModel!
     
     override func viewDidLoad() {
