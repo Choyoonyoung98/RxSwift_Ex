b1e30651bf9b07ee97a47db46b6b0eb03d489dae
Choyoonyoung98 <yoon980208@gmail.com>
2022-03-07 19:24:51 +0900

    [UPDATE] 메모 보기 화면 구현

--- RxMemo/RxMemo/ViewController/MemoDetailViewController.swift
+++ RxMemo/RxMemo/ViewController/MemoDetailViewController.swift
@@ -18,9 +18,33 @@ class MemoDetailViewController: UIViewController, ViewModelBindableType {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    
     }
     
     func bindViewModel() {
+        viewModel.title
+            .drive(navigationItem.rx.title)
+            .disposed(by: rx.disposeBag)
         
+        viewModel.contents
+            .bind(to: contentTableView.rx.items) { tableView, row, value in
+                switch  row {
+                case 0:
+                    if let cell = tableView.dequeueReusableCell(withIdentifier: "conentCell") {
+                        cell.textLabel?.text = value
+                        return cell
+                    }
+                    return UITableViewCell()
+                case 1:
+                    if let cell = tableView.dequeueReusableCell(withIdentifier: "dateCell") {
+                        cell.textLabel?.text  = value
+                        return cell
+                    }
+                    return UITableViewCell()
+                default:
+                    fatalError()
+                }
+            }
+            .disposed(by: rx.disposeBag)
     }
 }
--- RxMemo/RxMemo/ViewModel/MemoDetailViewModel.swift
+++ RxMemo/RxMemo/ViewModel/MemoDetailViewModel.swift
@@ -6,7 +6,34 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+import Action
 
-class MemoDetailViewModel {
+class MemoDetailViewModel: CommonViewModel {
+    let memo: Memo //이전 Scene에서의 메모가 전달
     
+    //날짜 -> 문자열로 전환하기 위한 formatter
+    private var formatter: DateFormatter = {
+        let f = DateFormatter()
+        f.locale = Locale(identifier: "ko_kr")
+        f.dateStyle = .medium
+        f.timeStyle = .medium
+        return f
+    }()
+    
+    //일반 Observable이 아닌 왜 BehaviorSubject?
+    //-> 메모보기 화면에서 편집 버튼을 누르고 편집한 후, 되돌아오면 편집 내용이 반영되어야한다.
+    //이렇게 하려면 새로운 문자열 배열을 방출해야 하기 때문
+    var contents: BehaviorSubject<[String]> //문자열 2개를 방출해야 하기 때문에 (content, 날짜)
+    
+    init(memo: Memo, title: String, sceneCoordinator: SceneCoordinatorType, storage: MemoStorageType) {
+        self.memo = memo
+        contents = BehaviorSubject<[String]>(value: [
+            memo.content,
+            formatter.string(from: memo.insertDate)
+        ])
+        
+        super.init(title: title, sceneCoordinator: sceneCoordinator, storage: storage)
+    }
 }
